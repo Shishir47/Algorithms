2868d6e58c50937bf6673f062d93d4e446298d02
Raufun Nasim Khan Chowdhury <44887399+Shishir47@users.noreply.github.com>
2023-01-27 03:34:30 +0600

    Add files via upload

---
+++ DynamicConnectivity/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+	public static void main(String [] args) {
+		QuickUnion qu= new QuickUnion(5);
+		
+		qu.union(0, 3);
+		qu.union(3, 1);
+		qu.union(1, 4);
+		System.out.println("Quick Find, Slow: ");
+		System.out.println(qu.connected(0, 2));
+		System.out.println(qu.connected(1, 4));
+		System.out.println(qu.connected(0, 4));
+		
+		System.out.println();
+		System.out.println("Quick Union, Faster than Quick Find: ");
+		QuickFind qf= new QuickFind(5);
+		
+		qf.union(1, 3);
+		qf.union(4, 1);
+		qf.union(1, 2);
+		
+		System.out.println(qf.connected(0, 2));
+		System.out.println(qf.connected(1, 4));
+		System.out.println(qf.connected(0, 4));
+		
+		System.out.println();
+		System.out.println("Weighted Quick Union, Faster than both: ");
+		WeightedQuickUnion wqu= new WeightedQuickUnion(5);
+		
+		wqu.union(1, 3);
+		wqu.union(4, 1);
+		wqu.union(1, 2);
+		
+		System.out.println(wqu.connected(0, 2));
+		System.out.println(wqu.connected(1, 4));
+		System.out.println(wqu.connected(0, 4));
+		
+		
+	}
+}
+
---
+++ DynamicConnectivity/src/QuickUnion.java
@@ -0,0 +1,25 @@
+
+public class QuickUnion {
+	int [] id;
+	QuickUnion(int n){
+		id=new int[n];
+		for(int i=0; i<n; i++) {
+			id[i]=i;
+		}
+	}
+	private int root(int i) {
+		while(i != id[i]) {
+			i=id[i];
+		}
+		return i;
+	}
+	public boolean connected(int p, int q) {
+		return root(p)==root(q);
+	}
+	public void union(int p, int q) {
+		int i=root(p);
+		int j=root(q);
+		id[i]=j;
+	}
+	
+}
